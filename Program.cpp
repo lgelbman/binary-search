7d6e534e6527bb2e2096f13d52c8159fd334346c
lgelbman <lgelbman@student.touro.edu>
2021-03-01 17:20:53 -0500

    completed part 2

--- Program.cpp
+++ Program.cpp
@@ -1,6 +1,16 @@
 #include <list>
+#include <vector>
 #include <iostream>
 #include <string.h>
+#include <algorithm>
+
+#include <chrono>
+using std::chrono::system_clock;
+using std::chrono::steady_clock;
+using std::chrono::duration_cast;
+using std::chrono::seconds;
+using std::chrono::milliseconds;
+
 using namespace std;
 
 template <class T>
@@ -30,39 +40,17 @@ T* recursiveBinarySearch(T array[], int size, T data) {
     }
 }
 
+
 template <class T>
 T* linkedBinarySearch(std::list<T> linkedList, T data) {
-
-    int startIndex = 0;
-    int endIndex = linkedList.size() - 1;
-
-    T* result = nullptr;
-    while (startIndex <= endIndex) {
-        int middleIndex = (startIndex + (endIndex - 1)) / 2;
-        int* ptr = getElementAt(linkedList, middleIndex);
-        if (*ptr == data) {
-            return ptr;
-        } else if (*ptr > data) {
-            endIndex = middleIndex - 1;
-        } else {
-            startIndex = middleIndex + 1;
-        }
-    } 
-    return result;
-
-}
-
-
-template <class T>
-T* linkedBinarySearch2(std::list<T> linkedList, T data) {
     int startIndex = 0;
     int endIndex = linkedList.size();
     T* ptr = 0;
     while (startIndex <= endIndex) {
         
-        if (endIndex - startIndex == 1) {
-            if (*getElementAt(linkedList, endIndex - startIndex) == data) {
-                return getElementAt(linkedList, endIndex - startIndex);
+        if (endIndex == startIndex) {
+            if (*getElementAt(linkedList, startIndex) == data) {
+                return getElementAt(linkedList, startIndex);
             }
             return 0;
         }
@@ -99,10 +87,56 @@ T* middleOfLinkedList(std::list<T> linkedList) {
 }
 
 
+void createAndSearchUsingList(){
+    const int amountOfData = 100000;
+    std::list<int> linkedList;
+    for (int i = 0; i < amountOfData; i++) {
+        linkedList.push_back(i);
+    }
+    for (int i = 0; i < amountOfData; i++) {
+        linkedBinarySearch(linkedList, i);
+    }
+}
 
+//copied from Timing Example.txt
+template <typename Func>
+long long TimeFunc(Func f){
+    auto begin = steady_clock::now();
+    f();
+    auto end = steady_clock::now();
+    return duration_cast<milliseconds>(end - begin).count();
+}
+
+const int amountOfData = 10000000;
+int nums[amountOfData];
+std::list<int> linkedList;
+
+void timedLinkedBinarySearch() {
+    for (int i = 0; i < amountOfData; i++) {
+        linkedBinarySearch(linkedList, i);
+    }
+}
+
+void timedRecursiveBinarySearch() {
+    for (int i = 0; i < amountOfData; i++) {
+        recursiveBinarySearch<int>(nums, amountOfData, i);
+    }
+}
 
 int main() {
     
+    
+    std::list<int> list;
+    for (int i = 0; i < amountOfData; i++) {
+        list.push_back(i);
+        nums[i] = i;
+    }
+    long long timeForLinked = TimeFunc(timedLinkedBinarySearch);
+    long long timeForRecursive = TimeFunc(timedRecursiveBinarySearch);
+    std::cout << "linked: " << timeForLinked << std::endl;
+    std::cout << "recursive: " << timeForRecursive << std::endl;
+
+
     /*
     int nums[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
     for (int i = 0; i <= 15; i += 5) {
@@ -113,7 +147,7 @@ int main() {
             cout << "element not found" << endl;
         }
     }
-    */
+    
  
     std::list<int> linkedList;
     for(int i = 0; i < 50; i+=2) {
@@ -149,10 +183,15 @@ int main() {
     } else {
         cout << result4 << endl;
     }
-
+    */
 
     
 }
+
+
+
+
+
 
 
 
